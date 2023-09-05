14215c5eee7b3579cdc08be0a0efdbd62e5c120c
Harsh Cheema <98507707+harsh-cheema@users.noreply.github.com>
2023-09-05 20:18:58 +0530

    Update quick_sort.c

--- PA Ritesh/Sorting/quick_sort.c
+++ PA Ritesh/Sorting/quick_sort.c
@@ -2,7 +2,7 @@
 #include <stdbool.h>
 
 int partition(int arr[],int l,int h){
-    /*int pivot=arr[h];
+    int pivot=arr[h];
     int j=l-1;
     int i;
     for(i=l;i<h;i++){
@@ -16,32 +16,14 @@ int partition(int arr[],int l,int h){
     int temp=arr[j+1];
     arr[j+1]=arr[h];
     arr[h]=temp;
-    return j+1;*/
-
-    int pivot=arr[l];
-    int i=l-1;
-    int j=h+1;
-    while(true){
-        do{
-            i++;
-        }while(arr[i]<pivot);
-        do{
-            j--;
-        }while(arr[j]>pivot);
-
-        if(i>=j) return j;
-        int temp=arr[i];
-        arr[i]=arr[j];
-        arr[j]=temp;
-
-    }
+    return j+1;
 }
 
 
 void quick_sort(int arr[],int l,int h){
     if(l>=h) return;
     int p=partition(arr,l,h);
-    quick_sort(arr,l,p);
+    quick_sort(arr,l,p-1);
     quick_sort(arr,p+1,h);
 }
 
